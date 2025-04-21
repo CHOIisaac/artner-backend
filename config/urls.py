5d83fb01b1ed53df6cc3d4b63751cb6c822f2f16
CHOIisaac <ehdrhkdls@gmail.com>
2025-04-21 20:44:19 +0900

    feat: url router 설정

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for artner project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -19,11 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
-# drf-yasg 관련 import 제거
-# from drf_yasg.views import get_schema_view
-# from drf_yasg import openapi
 from rest_framework import permissions
-# drf-spectacular 관련 import 추가
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 # 각 앱의 라우터 가져오기
@@ -31,7 +27,7 @@ from users.urls import router as users_router
 from exhibitions.urls import router as exhibitions_router
 from artworks.urls import router as artworks_router
 from docents.urls import router as docents_router
-from art_collections.urls import router as collections_router  # 수정: collections -> art_collections
+from art_collections.urls import router as collections_router
 from common.urls import router as common_router
 
 # 메인 라우터 생성
@@ -51,8 +47,8 @@ urlpatterns = [
     
     # drf-spectacular 관련 URL 추가
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('swagger', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 # 개발 환경에서 미디어 파일 서빙
--- docents/urls.py
+++ docents/urls.py
@@ -1,10 +1,9 @@
 from rest_framework.routers import DefaultRouter
 from .views import DocentViewSet, DocentItemViewSet, DocentHighlightViewSet
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 router.register(r'docents', DocentViewSet)
 router.register(r'docent-items', DocentItemViewSet)
-router.register(r'docent-highlights', DocentHighlightViewSet)
 router.register(r'highlights', DocentHighlightViewSet)
 
 urlpatterns = router.urls 
