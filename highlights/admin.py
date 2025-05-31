a7cae8bfa165667216755be068694e30ec54b573
CHOIisaac <ehdrhkdls@gmail.com>
2025-05-31 23:01:32 +0900

    feat: highlights admin 구현

--- highlights/admin.py
+++ highlights/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Highlight
 
-# Register your models here.
+@admin.register(Highlight)
+class HighlightAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content_type', 'object_id', 'order', 'is_active', 'created_at')
+    list_filter = ('is_active', 'content_type')
+    search_fields = ('title', 'description')
+    ordering = ('order', '-created_at')
