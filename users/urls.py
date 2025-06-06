f669ad266e4ef4991ce858884cdaa8ffc2d94d2b
CHOIisaac <ehdrhkdls@gmail.com>
2025-06-06 23:23:31 +0900

    feat: 유저선호 모델 삭제 후 기능 정리

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, UserPreferenceViewSet
+from .views import UserViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
-router.register(r'user-preferences', UserPreferenceViewSet) 
