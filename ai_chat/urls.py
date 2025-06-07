1630c35a59cf2003127396f4ea3d96efccadf3b2
CHOIisaac <ehdrhkdls@gmail.com>
2025-06-07 22:34:59 +0900

    feat: ai_chat 앱 삭제

--- ai_chat/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ChatView, ArtworkInfoView
-
-urlpatterns = [
-    path('chat', ChatView.as_view(), name='chat'),
-    path('artwork-info', ArtworkInfoView.as_view(), name='artwork-info'),
-] 
