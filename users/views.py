5751a074d08d845c3b49ed7b41985a1f79aa28a3
CHOIisaac <ehdrhkdls@gmail.com>
2025-06-09 19:58:54 +0900

    feat: 유저 내정보 보기 기능만 유지(커스텀)하고 나머지 삭제

--- users/views.py
+++ users/views.py
@@ -2,50 +2,26 @@ from django.shortcuts import render
 from rest_framework import viewsets, filters, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import User
 from .serializers import UserSerializer
-from common.mixins import DetailedSerializerMixin
 from drf_spectacular.utils import extend_schema, extend_schema_view
 
-# Create your views here.
 
 @extend_schema_view(
-    list=extend_schema(
-        description="사용자 목록을 조회합니다.",
-        tags=["Users"]
-    ),
-    retrieve=extend_schema(
-        description="사용자 상세 정보를 조회합니다.",
-        tags=["Users"]
-    ),
-    create=extend_schema(
-        description="새로운 사용자를 생성합니다.",
-        tags=["Users"]
-    ),
-    update=extend_schema(
-        description="사용자 정보를 업데이트합니다.",
-        tags=["Users"]
-    ),
-    partial_update=extend_schema(
-        description="사용자 정보를 부분 업데이트합니다.",
-        tags=["Users"]
-    ),
-    destroy=extend_schema(
-        description="사용자를 삭제합니다.",
+    me=extend_schema(
+        description="현재 로그인한 사용자 자신의 정보를 조회합니다.",
         tags=["Users"]
     )
 )
-class UserViewSet(DetailedSerializerMixin, viewsets.ModelViewSet):
-    queryset = User.objects.all()
+class UserViewSet(viewsets.GenericViewSet):
+    """사용자 본인 정보 조회만 제공하는 ViewSet"""
+    queryset = User.objects.all()  # 라우터 등록에 필요
     serializer_class = UserSerializer
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields = ['is_active', 'is_staff']
-    search_fields = ['username', 'email', 'nickname']
     
     @action(detail=False, methods=['get'])
     def me(self, request):
+        """현재 로그인한 사용자 정보 조회"""
         user = request.user
         if user.is_authenticated:
             serializer = UserSerializer(user)
