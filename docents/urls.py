6a4a329682cb420ea6e1df716c0b276ce5946efa
CHOIisaac <ehdrhkdls@gmail.com>
2025-06-18 21:54:45 +0900

    chore: DRF 라우터에서 trailing_slash=False 설정 추가

--- docents/urls.py
+++ docents/urls.py
@@ -2,13 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import FolderViewSet, DocentViewSet, generate_realtime_docent, get_audio_status, stream_audio
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 router.register(r'folders', FolderViewSet, basename='folder')
 router.register(r'docents', DocentViewSet, basename='docent')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('realtime-docent/', generate_realtime_docent, name='generate_realtime_docent'),
-    path('audio-status/<str:job_id>/', get_audio_status, name='get_audio_status'),
-    path('stream-audio/<str:job_id>/', stream_audio, name='stream_audio'),
+    path('realtime-docent', generate_realtime_docent, name='generate_realtime_docent'),
+    path('audio-status/<str:job_id>', get_audio_status, name='get_audio_status'),
+    path('stream-audio/<str:job_id>', stream_audio, name='stream_audio'),
 ] 
